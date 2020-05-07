e0a321d9267d6670547c543ed2832aef42efd1aa
skyfile <fengfilesky@gmail.com>
2020-05-07 09:20:54 +0800

    fix

--- src/Api/Link.php
+++ src/Api/Link.php
@@ -6,11 +6,11 @@ use KlZkSDK\Tools\GateWay;
 
 class Link extends GateWay
 {
-    const CPS_URL = 'https://cps.kaola.com/cps/zhuankeLogin?unionId=&tc1=&tc2=&showWapBanner=0&targetUrl=';
+    const CPS_URL = 'https://cps.kaola.com/cps/zhuankeLogin?showWapBanner=0';
 
     public function cps($targetUrl = '', $tc1 = '', $tc2 = '', $unionId = null)
     {
-        return self::CPS_URL . http_build_query([
+        return self::CPS_URL . '&' . http_build_query([
                 'unionId'   => $unionId ?: $this->unionId,
                 'tc1'       => $tc1,
                 'tc2'       => $tc2,
